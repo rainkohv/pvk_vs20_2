47b383979b9ec7da891a7b51c2778f395c51cd7e
Rain Kohv <rain.kohv@khk.ee>
2021-01-12 15:39:44 +0200

    Loetud lingiga edastatud andmed ja valmistatud ette nende töötluseks abifailid

---
+++ app/libs/Core.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Core
+{
+    //constructor
+
+    // get url data
+    /**
+     * Core constructor.
+     */
+    public function __construct()
+    {
+        $this->getUrl();
+    }
+
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = $_GET['url'];
+            echo $url;
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,2 +1,4 @@
 <?php
-echo 'PVK aine projekt';
+require_once '../app/bootstrap.php';
+//init appp core
+$init = new Core();
