e8acefb6283ff255ce9356dcaf3ac50a41883727
Rain Kohv <rain.kohv@khk.ee>
2021-01-12 15:49:20 +0200

    Lingi andmed on ettevalmistatud kasutuseks

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -17,7 +17,11 @@ class Core
     public function getUrl(){
         if(isset($_GET['url'])){
             $url = $_GET['url'];
-            echo $url;
+            $url = rtrim($url, '/');
+            $url = htmlentities($url);
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
         }
     }
 }
