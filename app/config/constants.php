b93428d2153a5c5f331cc7b8cddc0f8e844aa24e
Rain Kohv <rain.kohv@khk.ee>
2021-01-13 11:25:33 +0200

    Lisatud kasutusele rakendusega seotud konstandid.

---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+//project name
+define('PROJECT_NAME', 'pvk_vs20');
+//app root dir
+define('APPROOT', dirname(dirname(__FILE__)));
+//url root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
 require_once '../app/bootstrap.php';
+//conf test
+echo URLROOT;
 //init appp core
 $init = new Core();
