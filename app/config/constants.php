6ce4188b0810818ab5d0fb247222b57511226207
Rain Kohv <rain.kohv@khk.ee>
2021-01-14 12:32:59 +0200

    Loodud ja testitud ühendus andmebaasiga.

--- app/config/constants.php
+++ app/config/constants.php
@@ -5,3 +5,9 @@ define('PROJECT_NAME', 'pvk_vs20');
 define('APPROOT', dirname(dirname(__FILE__)));
 //url root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+//db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pvk_vs20');
---
+++ app/models/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Page
+{
+    private $db;
+
+    /**
+     * Page constructor.
+     */
+    public function __construct()
+    {
+        $this->db = new Database();
+        echo 'db is connected';
+    }
+
+}
